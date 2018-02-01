1fa7ab6213a63021fb40329bdb42b7f29ac75318
Vincent Cunningham <v.cunningham@flagrama.com>
2018-02-01 16:03:12 -0500

    Add AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyDungeon")]
+[assembly: AssemblyDescription("A Roguelike engine for Unity")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("flagrama")]
+[assembly: AssemblyProduct("MyDungeon")]
+[assembly: AssemblyCopyright("Copyright © 2018 Vincent Cunningham")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8571519-1148-42fb-9b7f-be6da268c2d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
