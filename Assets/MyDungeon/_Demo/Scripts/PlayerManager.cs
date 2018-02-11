d3b463407a080984083328edbf01f531bb67e6b3
Vincent Cunningham <v.cunningham@flagrama.com>
2018-02-11 18:09:27 -0500

    Remove XP from PlayerManager

--- Assets/MyDungeon/_Demo/Scripts/PlayerManager.cs
+++ Assets/MyDungeon/_Demo/Scripts/PlayerManager.cs
@@ -6,15 +6,11 @@ namespace MyDungeon.Demo
     public class PlayerManager : MonoBehaviour
     {
         public static PlayerManager Instance;
-
-        public int BaseXp = 100;
+        
         public int CurHealth;
-        public int CurXp;
-        public bool Initialized = false;
+        public bool Initialized;
         public int Level = 1;
-        public float LevelFactor = 2;
         public int MaxHealth;
-        public int NextXp;
         public string PlayerName;
 
         // Use this for initialization
@@ -33,40 +29,7 @@ namespace MyDungeon.Demo
             PlayerName = initPlayerName;
             MaxHealth = maxHp;
             CurHealth = MaxHealth;
-            NextXp = Instance.CalculateNextXp();
             GameManager.Inventory = new List<Item>();
-        }
-
-        private void LevelUp()
-        {
-            Level++;
-            UpdateLevel();
-            GameObject.FindGameObjectWithTag("HudManager").GetComponent<MessageLogDisplay>().AddMessage(PlayerName + " has reached level " + Level);
-        }
-
-        private int CalculateNextXp()
-        {
-            int xp = BaseXp * Mathf.RoundToInt(Mathf.Pow(Level, LevelFactor));
-
-            return xp;
-        }
-
-        private void UpdateLevel()
-        {
-            GameObject.FindGameObjectWithTag("HudManager").GetComponent<LevelDisplay>().UpdateLevel(Level);
-        }
-
-        public void GainXp(int xp)
-        {
-            CurXp += xp;
-            if (CurXp >= NextXp)
-            {
-                LevelUp();
-                CurXp -= NextXp;
-                NextXp = CalculateNextXp();
-            }
-
-            GameObject.FindGameObjectWithTag("HudManager").GetComponent<MessageLogDisplay>().AddMessage(PlayerName + " gained " + xp + " experience points");
         }
 
         public void Load(MySaveData saveData)
